b5696adf006a397402f568f703800063f5a20c1f
sansaovieira <vieirasansao42@gmail.com>
2025-08-22 13:30:10 -0300

    refactor: deploy Angular app - resolvendo erro cross

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,19 @@
+// api/proxy.js
 import fetch from "node-fetch";
 
 export default async function handler(req, res) {
   try {
     const url = `https://challenge-backend.mobi7.io${req.url}`;
     const response = await fetch(url);
+
+
     const data = await response.json();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Content-Type", "application/json");
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
     res.status(200).json(data);
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: "Erro no proxy" });
+    res.status(500).json({ error: 'Erro no proxy' });
   }
 }
